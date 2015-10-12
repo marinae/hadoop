db2b4a6c6a6b819892690ee1be1ad540f521f630
Marina Ermakova <m.ermakova77@gmail.com>
2015-10-13 01:59:27 +0300

    Java

---
+++ hw1/runJava.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+hadoop fs -rm -r hw1_java
+
+javac -d classes/ PatentStatistic.java && \
+jar -cvf patentstatistic.jar -C classes/ ./ && \
+hadoop jar patentstatistic.jar org.myorg.PatentStatistic /data/patents/apat63_99.txt hw1_java
